822d062eff64baaea494049c730c7a8db9e0568a
roeyk <roeykelner@gmail.com>
2022-04-08 02:45:45 +0300

    Submitted.

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -12,6 +12,11 @@ import plotly.io as pio
 pio.templates.default = "simple_white"
 
 
+# pd.set_option("max_columns", None)  # show all cols
+# pd.set_option('max_colwidth', None)  # show full width of showing cols
+# pd.set_option("expand_frame_repr", False)  # print cols side by side as it's supposed to be
+
+
 def load_data(filename: str) -> pd.DataFrame:
     """
     Load city daily temperature dataset and preprocess data.
@@ -25,19 +30,13 @@ def load_data(filename: str) -> pd.DataFrame:
     Design matrix and response vector (Temp)
     """
 
-    # todo remove
-
-    pd.set_option("max_columns", None)  # show all cols
-    pd.set_option('max_colwidth', None)  # show full width of showing cols
-    pd.set_option("expand_frame_repr", False)  # print cols side by side as it's supposed to be
-
     df = pd.read_csv(filename, parse_dates=['Date'])
 
+    # Adding the day of year column
     df['DayOfYear'] = df['Date'].dt.day_of_year
 
     # Removing samples with invalid values
     df = df[(df['Temp'] > -30)]
-
     return df
 
 
@@ -45,38 +44,34 @@ if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
 
-    #
-    # # Question 2 - Exploring data for specific country
+    # Question 2 - Exploring data for specific country
     all_df = load_data("../datasets/City_Temperature.csv")
     il_df = all_df[all_df['Country'] == "Israel"]
     il_df["Year"] = il_df["Year"].astype(str)
-    # il_df["Month"] = il_df["Month"].astype(str)
-    fig1 = px.scatter(x=il_df['DayOfYear'], y=il_df['Temp'], color=il_df['Year'])
+    fig1 = px.scatter(x=il_df['DayOfYear'], y=il_df['Temp'], color=il_df['Year'],
+                      labels={'x': "Day of the year", 'y': 'Temperature (Cel.)', 'color': 'Year'})
+    fig1.show()
 
-    # fig1.show()
-
+    # Grouping by month and temperature
     months_std_df = il_df[['Month', 'Temp']].groupby('Month').agg('std').reset_index()
 
-    fig2 = px.bar(x=months_std_df['Month'], y=months_std_df['Temp'], labels={'x': "Month", 'y': "Temperature (Cel.)"})
-    # fig2.update_layout(showlegend = False)
+    fig2 = px.bar(x=months_std_df['Month'], y=months_std_df['Temp'], labels={'x': "Month", 'y': "Temperature (Cel.)"},
+                  title="STD of temperature in Israel month")
+    # Making the X axis categorical rather than continuous
     fig2.update_xaxes(type='category')
-    # fig2.show()
+    fig2.show()
 
-    # print(months_std_df)
-
-    # # Question 3 - Exploring differences between countries
+    # Question 3 - Exploring differences between countries
 
     country_df = all_df[['Country', 'Month', 'Temp']].groupby(['Country', 'Month']).agg(['mean', 'std'])
     country_df = country_df.reset_index()
-    # print(country_df)
-    #
-    # print(country_df.info())
+
     fig3 = px.line(x=country_df[('Month',)], y=country_df[('Temp', 'mean')], color=country_df[('Country',)],
                    error_y=country_df[('Temp', 'std')],
-                   labels={'x': "Month", 'y': "Temperature (Cel.)", 'color': 'Country'})
+                   labels={'x': "Month", 'y': "Temperature (Cel.)", 'color': 'Country'}
+                   , title="Monthly mean temperature, seperated by country")
     fig3.update_xaxes(type='category')
-    # todo return
-    # fig3.show()
+    fig3.show()
 
     # Question 4 - Fitting model for different values of `k`
     y = il_df['Temp']
@@ -84,7 +79,7 @@ if __name__ == '__main__':
     train_X, train_y, test_X, test_y = split_train_test(X, y)
     for item in [train_X, train_y, test_X, test_y]:
         item = np.array(item)
-    # print(train_X, train_y, test_X, test_y, sep="\n\n")
+
     losses = []
     for k in range(1, 11):
         poly_est = PolynomialFitting(k)
@@ -96,7 +91,7 @@ if __name__ == '__main__':
                   labels={'x': "Polynomial degree fitted", 'y': "MSE"},
                   title="MSE as a function of the polynomial degree used for fitting")
     fig4.update_xaxes(type='category')
-    # fig4.show()
+    fig4.show()
 
     # # Question 5 - Evaluating fitted model on different countries
     il_model = PolynomialFitting(6)
